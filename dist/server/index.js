975ab291628ab3f01b07856ff653d20148f6819c
New-Horizon-IT <support@newhorizon-it.co.uk>
2026-06-04 23:55:39 +0100

    chore: stop tracking dist/ build output (it's gitignored)

--- dist/server/index.js
+++
@@ -1,8 +0,0 @@
-import { aj } from "./assets/worker-entry-O2i-Fupu.js";
-import "node:events";
-import "node:async_hooks";
-import "node:stream/web";
-import "node:stream";
-export {
-  aj as default
-};
